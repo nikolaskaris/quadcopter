768b7045366e4903a409b648d4e4f7aa3b047b73
Niko Laskaris <laskarisnik@gmail.com>
2018-07-10 11:34:17 -0400

    update

--- task.py
+++ task.py
@@ -3,8 +3,8 @@ from physics_sim import PhysicsSim
 
 class Task():
     """Task (environment) that defines the goal and provides feedback to the agent."""
-    def __init__(self, init_pose=None, init_velocities=None, 
-        init_angle_velocities=None, runtime=5., target_pos=None):
+    def __init__(self, init_pose=None, init_velocities=None,
+        init_angle_velocities=None, runtime=10., target_pos=None):
         """Initialize a Task object.
         Params
         ======
@@ -15,21 +15,48 @@ class Task():
             target_pos: target/goal (x,y,z) position for the agent
         """
         # Simulation
-        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime)
+        self.start_pos = self.sim.pose[:3]
         self.action_repeat = 3
 
-        self.state_size = self.action_repeat * 6
-        self.action_low = 0
-        self.action_high = 900
-        self.action_size = 4
+        # state made of current position, velocity and angular velocity
+        self.state_size = self.action_repeat * (6 + 3 + 3)
+        self.action_low = 0    # default 0
+        self.action_high = 900 # default 900
+        self.action_size = 4   # default 4
 
         # Goal
-        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
-        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        reward = 0
+        penalty = 0
+        current_position = self.sim.pose[:3]
+        # penalty for euler angles, we want the takeoff to be stable
+        # penalty += 2*abs(self.sim.pose[3:6]).sum()
+        
+        # link velocity to residual distance
+        # penalty += abs(abs(current_position-self.target_pos).sum() - abs(self.sim.v).sum())
+        
+        squared_x_diff = abs(current_position[0]-self.target_pos[0])**2
+        squared_y_diff = abs(current_position[1]-self.target_pos[1])**2
+        squared_z_diff = abs(current_position[2]-self.target_pos[2])**2
+        
+        # penalty for distance from target
+        penalty += squared_x_diff + squared_y_diff + 10*squared_z_diff
+
+        distance = np.sqrt((current_position[0]-self.target_pos[0])**2 + (current_position[1]-self.target_pos[1])**2 + (current_position[2]-self.target_pos[2])**2)
+        
+        # extra reward for flying near the target        
+        reward += (75 - distance) * 10
+
+        # constant reward for flying
+        reward += 100
+        
+        reward = reward - penalty*0.00002
         return reward
+
 
     def step(self, rotor_speeds):
         """Uses action to obtain next state, reward, done."""
@@ -37,13 +64,19 @@ class Task():
         pose_all = []
         for _ in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
-            reward += self.get_reward() 
-            pose_all.append(self.sim.pose)
+            reward += self.get_reward()
+            state = self.current_state()
+            pose_all.append(self.current_state())
         next_state = np.concatenate(pose_all)
         return next_state, reward, done
+
+    def current_state(self):
+        """The state contains information about current position, velocity and angular velocity"""
+        state = np.concatenate([np.array(self.sim.pose), np.array(self.sim.v), np.array(self.sim.angular_v)])
+        return state
 
     def reset(self):
         """Reset the sim to start a new episode."""
         self.sim.reset()
-        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        state = np.concatenate([self.current_state()] * self.action_repeat)
         return state
